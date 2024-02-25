6071cf5fd71ff631bc000d2b95187b87b2dd9b1e
ashish082003 <aj9172243856@gmail.com>
2024-02-25 12:51:27 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import firebase from 'firebase/app';// firebase sdk
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import {useAuthState} from 'react-firebase-hooks/auth';// hooks
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC56dxd_Xd7a2ItLaqA7kr7dWbt6MrywXE",
+  authDomain: "chat-app-2f0d2.firebaseapp.com",
+  projectId: "chat-app-2f0d2",
+  storageBucket: "chat-app-2f0d2.appspot.com",
+  messagingSenderId: "1011416772697",
+  appId: "1:1011416772697:web:1eb700704f7e94b1df8148",
+  measurementId: "G-TP5WJDXQTY"
+})
+
+const [user]=useAuthState(auth);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );
